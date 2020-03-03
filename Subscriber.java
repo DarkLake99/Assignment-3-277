ab53f960864599d03e18fed12c14afcfa52e303b
Charyl Enriquez <charyl.enriquez@student.csulb.edu>
2020-03-03 13:07:55 -0800

    added changes to subscriber and venue

--- Subscriber.java
+++ Subscriber.java
@@ -1,4 +1,13 @@
-
-public class Subscriber {
+import java.util.Calendar;
+public class Subscriber 
+{
+	public Subscriber(String name_input, String date_input) {
+	 	mName = name_input;
+		mBirthDate = DateUtil.convertStringDatetoCalendarDate(date_input);
+	}
+	
+	
+	private String mName;
+	private Calendar mBirthDate;
 
 }
--- Venue.java
+++ Venue.java
@@ -1,4 +1,13 @@
 
-public class Venue implements Comparable,Displayable {
-
+public class Venue implements Comparable {
+	private String mName;
+	private int mCapacity;
+	public Venue(String name_input, int capacity_input) {
+		mName = name_input;
+		mCapacity = capacity_input;
+	}
+	@Override
+	public int compareTo(Venue v) {
+		return mCapacity - v.mCapacity;
+	}
 }
